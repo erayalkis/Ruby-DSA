b322eaac8015c637bd47c30bde279c738fb00862
erayalkis <erayalkis@hotmail.com>
2022-06-01 22:26:07 +0300

    Apply rubocop changes

---
+++ 0 - Basics/best_time_to_buy_sell_stock/best_time_to_buy_sell_stock.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def best_time_to_buy_sell_stock(prices)
+  left = 0
+  right = 1
+  profit = 0
+
+  while right > prices.length
+    curr_profit = prices[right] - prices[left]
+
+    if prices[right] > prices[left]
+      profit = curr_profit if curr_profit > profit
+    else
+      left = right
+    end
+
+    right += 1
+  end
+
+  profit
+end
