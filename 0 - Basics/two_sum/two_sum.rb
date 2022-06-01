9f0e2f19b0268059a0bb21828d37a7187d032eb3
erayalkis <erayalkis@hotmail.com>
2022-06-01 21:59:32 +0300

    Add safeguard for nil arguments in #two_sum

--- 0 - Basics/two_sum/two_sum.rb
+++ 0 - Basics/two_sum/two_sum.rb
@@ -9,6 +9,8 @@
 # Time/Space Complexity: O(n^2) Time / O(1) Space
 
 def two_sum(nums, target)
+  return [-1, -1] unless nums && target
+
   (0...nums.length).each do |i|
     (i + 1...nums.length).each do |j|
       return [i, j] if nums[i] + nums[j] == target
