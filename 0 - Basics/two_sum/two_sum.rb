ff37c0b5e9264d3e578ddc9e46e6a10503ed41c8
erayalkis <erayalkis@hotmail.com>
2022-06-01 19:54:40 +0300

    Add Time/Space complexity to two_sum

--- 0 - Basics/two_sum/two_sum.rb
+++ 0 - Basics/two_sum/two_sum.rb
@@ -6,6 +6,7 @@
 # return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
+# Time/Space Complexity: O(n^2) Time / O(1) Space
 
 def two_sum(nums, target)
   (0...nums.length).each do |i|
