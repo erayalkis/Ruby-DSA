9e7f16054e2243c57bb2f67cbdef0a082a707b7c
erayalkis <erayalkis@hotmail.com>
2022-06-02 23:04:53 +0300

    Create example_solution.rb

---
+++ example_solution.rb
@@ -0,0 +1,12 @@
+# -- Problem Name: Example Solution --
+# -- Description: --
+# Given two integers, return their sum.
+# -- Time/Space Complexity: --
+# O(1) Time / O(1) Space
+# -- Additional Context: --
+# Put any other additional context here! For example, if you're optimizing an already existing solution,
+# you should state it here.
+
+def example_method(int1, int2)
+  int1 + int2
+end
