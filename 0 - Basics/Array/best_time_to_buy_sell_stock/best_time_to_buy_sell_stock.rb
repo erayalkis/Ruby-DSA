3d517ba5b74b5e257399e9c0104bbec87ed07138
erayalkis <erayalkis@hotmail.com>
2022-06-02 21:02:55 +0300

    Add description for best_time_to_buy_sell_stock

--- 0 - Basics/Array/best_time_to_buy_sell_stock/best_time_to_buy_sell_stock.rb
+++ 0 - Basics/Array/best_time_to_buy_sell_stock/best_time_to_buy_sell_stock.rb
@@ -1,3 +1,12 @@
+# -- Problem Name: Best Time to Buy and Sell Stock --
+# -- Description: --
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy one stock
+# and choosing a different day in the future to sell that stock.
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0
+# -- Time/Space Complexity: --
+# O(n) Time / O(1) Space
+
 def best_time_to_buy_sell_stock(prices)
   left = 0
   right = 1
