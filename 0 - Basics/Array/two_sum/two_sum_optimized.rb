b6f864d6c0abedc03025846cffa2f2f5df43bf63
erayalkis <erayalkis@hotmail.com>
2022-06-02 20:59:47 +0300

    Update description for two_sum_optimized

--- 0 - Basics/Array/two_sum/two_sum_optimized.rb
+++ 0 - Basics/Array/two_sum/two_sum_optimized.rb
@@ -1,13 +1,14 @@
-# Problem Name: Two Sum
-# Description:
+# -- Problem Name: Two Sum --
+# -- Description: --
 # Given an array of integers nums and an integer target,
 # return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
-# Additional Context:
+# -- Time/Space Complexity: --
+# O(n) Time / O(n) Space
+# -- Additional Context: --
 # This is an optimized version of the original Two Sum solution. It uses a hash to store previously seen numbers
 # to avoid running two for loops.
-# Time/Space Complexity: O(n) Time / O(n) Space
 
 def two_sum_optimized(nums, target)
   return [-1, -1] unless nums && target
