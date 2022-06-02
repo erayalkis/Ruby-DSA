741c8c023f0639089495d574fc354fdab60d287e
erayalkis <erayalkis@hotmail.com>
2022-06-02 20:58:59 +0300

    Update example description text

--- 0 - Basics/Array/two_sum/two_sum.rb
+++ 0 - Basics/Array/two_sum/two_sum.rb
@@ -1,10 +1,11 @@
-# Problem Name: Two Sum
-# Description:
+# -- Problem Name: Two Sum --
+# -- Description: --
 # Given an array of integers nums and an integer target,
 # return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
-# Time/Space Complexity: O(n^2) Time / O(1) Space
+# -- Time/Space Complexity: --
+# O(n^2) Time / O(1) Space
 
 def two_sum(nums, target)
   return [-1, -1] unless nums && target
