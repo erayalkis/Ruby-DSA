cb5a6e6c29658d7c034319c3f60b98c68a44cf52
erayalkis <erayalkis@hotmail.com>
2022-06-03 15:35:13 +0300

    Write tests for #search_in_rotated_sorted

---
+++ spec/0 - Basics/Sorting and Searching/search_in_rotated_sorted/search_in_rotated_sorted_spec.rb
@@ -0,0 +1,15 @@
+require './0 - Basics/Sorting and Searching/search_in_rotated_sorted/search_in_rotated_sorted'
+
+describe '#search_in_rotated_sorted' do
+  it 'should find the targets index correctly' do
+    expect(search_in_rotated_sorted([4, 5, 6, 7, 0, 1, 2], 1)).to eq(6)
+  end
+
+  it 'should return -1 for an empty array' do
+    expect(search_in_rotated_sorted([], 1)).to eq(-1)
+  end
+
+  it 'should return -1 when target isn\'t in array' do
+    expect(search_in_rotated_sorted([4, 5, 6, 7, 0, 1, 2], 3)).to eq(-1)
+  end
+end
