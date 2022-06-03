8d5ff98056bfdbdf57b9c2d890866559f2b4a0a9
erayalkis <erayalkis@hotmail.com>
2022-06-04 02:58:09 +0300

    Fix tests for #search_in_rotated_sorted

--- spec/0 - Basics/Sorting and Searching/search_in_rotated_sorted/search_in_rotated_sorted_spec.rb
+++ spec/0 - Basics/Sorting and Searching/search_in_rotated_sorted/search_in_rotated_sorted_spec.rb
@@ -2,7 +2,7 @@ require './0 - Basics/Sorting and Searching/search_in_rotated_sorted/search_in_r
 
 describe '#search_in_rotated_sorted' do
   it 'should find the targets index correctly' do
-    expect(search_in_rotated_sorted([4, 5, 6, 7, 0, 1, 2], 1)).to eq(6)
+    expect(search_in_rotated_sorted([4, 5, 6, 7, 0, 1, 2], 1)).to eq(5)
   end
 
   it 'should return -1 for an empty array' do
